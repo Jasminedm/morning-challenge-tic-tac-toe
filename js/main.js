dc690b632e155c148b2f164587904d3c90245fe0
Jasminedm <mooredjasminedm01@gmail.com>
2022-10-11 22:40:16 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //Goal: Create a two player Tic-Tac-Toe game. The users should be able to 
 //click to place their X or O and 
 //if they win the program should mention their win in the DOM. 
-//Please make the game as OOP as possible.
+//Please make the game as OOP as possible.  QUESTIONS ON AS OOP AS POSSIBLE
 //Player 1 will be X
 //anysquare player1 clicks will display X
 //Player 2 will be O
